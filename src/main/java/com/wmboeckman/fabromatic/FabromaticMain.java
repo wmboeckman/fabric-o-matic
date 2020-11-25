0585c8679aa19246f5f139eb85a3fd18e53a890b
William Boeckman <wmboeckman@gmail.com>
2020-11-24 18:50:23 -0600

    Created better boilerplate entrypoint.

--- src/main/java/com/wmboeckman/fabromatic/FabromaticMain.java
+++ src/main/java/com/wmboeckman/fabromatic/FabromaticMain.java
@@ -1,13 +1,34 @@
 package com.wmboeckman.fabromatic;
 
+import com.wmboeckman.fabromatic.init.BlockInit;
+import com.wmboeckman.fabromatic.init.EntityInit;
+import com.wmboeckman.fabromatic.init.ItemInit;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import me.sargunvohra.mcmods.autoconfig1u.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class FabromaticMain implements ModInitializer {
+
+    public static final String MODID = "fabromatic";
+    private static final Logger LOGGER = LogManager.getLogger(MODID);
+
+    public static ModConfig config;
+
+
     @Override
     public void onInitialize() {
+        LOGGER.info("Starting Fabromatic Init...");
+
         AutoConfig.register(ModConfig.class, JanksonConfigSerializer::new);
-        ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+        config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+
+        BlockInit.register();
+        ItemInit.register();
+        EntityInit.register();
+
+        LOGGER.info("Finished registering Fabromatic content!");
     }
 }
